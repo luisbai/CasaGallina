e0e862821954dbd7902d53074151afe7e0cb89dc
luisbai <luis@casagallina.org.mx>
2026-06-17 15:38:19 -0600

    Update EstrategiasToProgramasSeeder.php

--- database/seeders/EstrategiasToProgramasSeeder.php
+++ database/seeders/EstrategiasToProgramasSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\DB;
 use App\Models\Estrategia;
 
 class EstrategiasToProgramasSeeder extends Seeder
