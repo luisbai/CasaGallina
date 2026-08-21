04f68e2e7836debdf11ccaadd12570bf8c836f98
Ketzalikab <blancoka878@gmail.com>
2026-08-21 17:57:57 -0600

    fix(subscribe.blade.php, suscripcion-boletin.blade.php):Se arregla vista responsive para nueva vista de suscripcion

--- resources/views/public/english/subscribe.blade.php
+++ resources/views/public/english/subscribe.blade.php
@@ -11,11 +11,12 @@
 
     <!-- Form Section -->
     <section class="py-8">
-        <div class="container mx-auto">
-            <div class="flex flex-col max-w-xl mx-auto px-4">
-                <div class="text-center border-b-2 border-green-600 pb-2 mb-4">
-             <span class="bg-green-600 px-8 py-2 text-white font-serif text-2xl leading-4">Subscribe to our newsletter</span>
-                </div>
+    <div class="container mx-auto">
+        <div class="flex flex-col max-w-xl mx-auto px-4">
+            <div class="text-center border-b-2 border-green-600 pb-2 mb-4">
+                <span class="inline-block bg-green-600 px-6 sm:px-8 py-2 text-white font-serif text-xl sm:text-2xl leading-normal">Subscribe to Newsletter</span>
+            </div>
+
 
                 <!-- Livewire Component (si requiere el idioma, se le puede pasar el parámetro: ['language' => 'en']) -->
                 <div class="w-full pt-4">
--- resources/views/public/suscripcion-boletin.blade.php
+++ resources/views/public/suscripcion-boletin.blade.php
@@ -12,11 +12,11 @@
 
     <!-- Form Section -->
     <section class="py-8">
-        <div class="container mx-auto">
-            <div class="flex flex-col max-w-xl mx-auto px-4">
-                <div class="text-center border-b-2 border-green-600 pb-2 mb-4">
-                    <span class="bg-green-600 px-8 py-2 text-white font-serif text-2xl leading-4">Suscríbete al boletín</span>
-                </div>
+    <div class="container mx-auto">
+        <div class="flex flex-col max-w-xl mx-auto px-4">
+            <div class="text-center border-b-2 border-green-600 pb-2 mb-4">
+                <span class="inline-block bg-green-600 px-6 sm:px-8 py-2 text-white font-serif text-xl sm:text-2xl leading-normal">Suscríbete al boletín</span>
+            </div>
 
                 <!-- Aquí se llama al componente de Livewire -->
                 <div class="w-full pt-4">
